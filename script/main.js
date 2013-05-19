8c34fe4c9b0d285f6ddfa0459d7b810f44c7ad80
phi <phi1618jp@gmail.com>
2013-05-19 23:13:36 +0900

    first commit

---
+++ script/main.js
@@ -0,0 +1,14 @@
+/*
+ * @author phi_jp
+ */
+
+
+(function() {
+	var $query    = function(q)  { return document.querySelector(q); }
+	var $queryAll = function(q)  { return document.querySelectorAll(q); }
+
+	window.onload = function() {
+
+	};
+})();
+
